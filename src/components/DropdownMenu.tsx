fadc575c849adfd26531e246d56d9ea6b7eea1d5
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2025-07-26 15:48:42 +0200

    Edited links

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function DropdownMenu() {
@@ -9,24 +10,24 @@ export default function DropdownMenu() {
         <nav className="dropdown absolute top-12 right-4 z-50 bg-[var(--white)] border border-[var(--light-grey)] rounded-md p-4">
             <ul className="flex-col">
                 <li className="py-1">
-                    <a href="/" className={path === "/" ? "active" : ""}>
+                    <Link href="/" className={path === "/" ? "active" : ""}>
                         Home
-                    </a>
+                    </Link>
                 </li>
                 <li className="py-1">
-                    <a href="/about" className={path === "/about" ? "active" : ""}>
+                    <Link href="/about" className={path === "/about" ? "active" : ""}>
                         About
-                    </a>
+                    </Link>
                 </li>
                 <li className="py-1">
-                    <a href="/contact" className={path === "/contact" ? "active" : ""}>
+                    <Link href="/contact" className={path === "/contact" ? "active" : ""}>
                         Contact
-                    </a>
+                    </Link>
                 </li>
                 <li className="py-1">
-                    <a href="/blog" className={path === "/blog" ? "active" : ""}>
+                    <Link href="/blog" className={path === "/blog" ? "active" : ""}>
                         Blog
-                    </a>
+                    </Link>
                 </li>
             </ul>
         </nav>
